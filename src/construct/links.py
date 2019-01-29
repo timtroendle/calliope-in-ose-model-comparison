505c0d6402b7bb717954f7a32cf11c29cb00d091
Tim Tröndle <tim.troendle@usys.ethz.ch>
2019-01-29 14:33:59 +0100

    Add one directional links

--- src/construct/links.py
+++ src/construct/links.py
@@ -1,16 +1,35 @@
 """Create Calliope link file."""
-from pathlib import Path
-
+import jinja2
 import pandas as pd
 import pycountry
-from calliope import AttrDict
+
+TEMPLATE = """
+links:
+    {% for row_id, row in ntcs.iterrows() %}
+    {{ row.fromLocation }},{{ row.toLocation }}.techs:
+        ac_transmission:
+            constraints:
+                energy_cap_equals: {{ row.capacity_go }}
+                one_way: true
+    {{ row.toLocation }},{{ row.fromLocation }}.techs:
+        ac_transmission:
+            constraints:
+                energy_cap_equals: {{ row.capacity_return }}
+                one_way: true
+    {% endfor %}
+"""
+
+# TODO allow for transmission capacity extension?
 
 
 def generate_links(path_to_ntc, path_to_result):
     """Generate a file that represents links in Calliope."""
     ntcs = _read_ntcs(path_to_ntc)
-    locations = _generate_links(ntcs)
-    locations.to_yaml(Path(path_to_result))
+    links = jinja2.Template(TEMPLATE).render(
+        ntcs=ntcs
+    )
+    with open(path_to_result, "w") as result_file:
+        result_file.write(links)
 
 
 def _read_ntcs(path_to_ntc):
@@ -21,28 +40,14 @@ def _read_ntcs(path_to_ntc):
     data["toLocation"] = data["Border"].map(
         lambda x: pycountry.countries.lookup(x.split("-")[1][:2]).alpha_3
     )
-    data["capacity"] = data.min(axis=1) * 1e3 # from MW to # TODO make directional
+    data["capacity_go"] = data["=>"] * 1e3 # from MW to kW
+    data["capacity_return"] = data["<="] * 1e3 # from MW to kW
+    # FIXME Northern Ireland is broken
     data.drop(columns=["Border", "=>", "<="], inplace=True)
     data.drop(index=data[data["fromLocation"] == data["toLocation"]].index, inplace=True)
-    return data.groupby(["fromLocation", "toLocation"]).capacity.sum().reset_index()
-
-
-def _generate_links(ntcs):
-    config = AttrDict()
-    for _, row in ntcs.iterrows():
-        config.union(
-            AttrDict.from_yaml_string(
-                f"""
-                links:
-                    {row.fromLocation},{row.toLocation}.techs:
-                        ac_transmission:
-                            constraints:
-                                energy_cap_equals: {row.capacity}
-                """
-                # TODO allow for transmission capacity extension?
-            )
-        )
-    return config
+    return (data.groupby(["fromLocation", "toLocation"])
+                .agg({"capacity_go": "sum", "capacity_return": "sum"})
+                .reset_index())
 
 
 if __name__ == "__main__":
